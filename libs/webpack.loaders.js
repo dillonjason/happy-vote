f00a393a16bfaeaa719a6a352b0db118a54740de
dillon <djasuar@gmail.com>
2016-08-27 18:13:12 -0400

    React Starter Import

---
+++ libs/webpack.loaders.js
@@ -0,0 +1,69 @@
+const webpack = require('webpack');
+
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+exports.styleLoaders = function(paths) {
+    return {
+        module: {
+            loaders: [
+                {
+                    test: /\.css$/,
+                    loaders: ['style', 'css'],
+                    include: paths
+                },
+                {
+                    test: /\.s(c|a)ss$/,
+                    loaders: ['style', 'css', 'sass'],
+                    include: paths
+                },
+                {
+                    test: /\.(jpg|png)$/,
+                    loader: 'file?name=[path][name].[hash].[ext]',
+                    include: paths.images
+                }
+            ]
+        }
+    }
+};
+
+exports.styleExtracts = function(paths) {
+    return {
+        module: {
+            loaders: [
+                {
+                    test: /\.css$/,
+                    loader: ExtractTextPlugin.extract('style', 'css!postcss'),
+                    include: paths.styles
+                },
+                {
+                    test: /\.s(c|a)ss$/,
+                    loader: ExtractTextPlugin.extract("style", "css!postcss!sass"),
+                    include: paths.styles
+                },
+                {
+                    test: /\.(jpg|png)$/,
+                    loader: 'file?name=[path][name].[hash].[ext]',
+                    include: paths.images
+                }
+            ]
+        },
+        plugins: [
+            new ExtractTextPlugin('[name].[chunkhash].css')
+        ]
+    }
+};
+
+exports.es6Loader = function(paths) {
+    return {
+        module: {
+            loaders: [
+                {
+                    test: /\.jsx?$/,
+                    exclude: /node_modules/,
+                    loader: 'babel',
+                    include: paths
+                }
+            ]
+        }
+    }
+};
